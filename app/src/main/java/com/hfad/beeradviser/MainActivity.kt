f65455418af375d757184a315b44e50060152070
Daniil Polyakov <polyakovd194@gmail.com>
2024-10-16 00:33:40 +0700

    Программирование основнйо логики разрабатываемого приложения

--- app/src/main/java/com/hfad/beeradviser/MainActivity.kt
+++ app/src/main/java/com/hfad/beeradviser/MainActivity.kt
@@ -1,47 +1,36 @@
+
 package com.hfad.beeradviser
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.hfad.beeradviser.ui.theme.BeerAdviserTheme
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            BeerAdviserTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val beerList = getBeers(color.toString())
+            val beers = beerList.reduce { str, item -> str + '\n' + item }
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = beers
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BeerAdviserTheme {
-        Greeting("Android")
+    fun getBeers(color: String): List<String> {
+        return when (color)
+        {
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
+        }
     }
 }
